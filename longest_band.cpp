9e286b0d0a4abfdbb950b43d675afa7eba3f03c3
Jay Punekar <jay.punekar@icloud.com>
2023-11-08 12:15:40 +0530

    added longestBand question only && .gitignore file

---
+++ longest_band.cpp
@@ -0,0 +1,28 @@
+/*
+Given an array containing N integers, find the length of the longest band.
+
+A band is defined as the longest subsequence which can be reordered in such a manner that all
+elements appear consicutive (i.e. with absolute difference of 1 between neighbouring elements)
+
+Input
+[1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 6]
+
+Output
+[8]
+
+Explaination
+Largest subsequence containing consecutive numbers is [0, 1, 2, 3, 4, 5, 6, 7]
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int main() {
+    vector<int> v = {1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6};
+
+    return 0;
+}
